407a6e34e3c30f7bee569a8d4404874261c59462
Marie Tranchant <marie.tranchant@maif.fr>
2026-01-23 14:27:15 +0100

    add navigate

--- src/pages/Edit.tsx
+++ src/pages/Edit.tsx
@@ -1,11 +1,12 @@
 import { updateContrat } from "../services/contrats";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import FormContrat from "../features/contrats/FormContrat";
 import type { Contrat } from "../types";
 
 const Edit = () => {
   const queryClient = useQueryClient();
+  const navigate = useNavigate();
   const { id } = useParams();
 
   const { mutate: fetchUpdateContrat } = useMutation({
@@ -17,6 +18,7 @@ const Edit = () => {
 
   const handleSubmit = (contrat: Omit<Contrat, "id"> | Contrat) => {
     fetchUpdateContrat({ ...contrat, id } as Contrat);
+    navigate("/");
   };
 
   return <FormContrat onSubmit={handleSubmit} />;
